fc37e3da82de267957375b06814cc6e4a5e4b3b5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-07 21:56:06 +0530

    app.jsx added java questions

--- BookAPI/my-react-app/src/App.jsx
+++ BookAPI/my-react-app/src/App.jsx
@@ -17,3 +17,46 @@ function App() {
 }
 
 export default App
+
+
+// 1. Why is "String" immutable in Java?
+
+// 2. How does "HashMap" work internally?
+
+// 3. What happens when two keys have the same hashcode?
+
+// 4. Why must "equals()" and "hashCode()" follow a contract?
+
+// 5. "ArrayList" vs "LinkedList" — when would you use each?
+
+// 6. "HashMap" vs "ConcurrentHashMap" — what's the real difference?
+
+// 7. What happens if you modify a collection while iterating over it?
+
+// 8. "synchronized" vs "volatile" — when would you use each?
+
+// 9. How would you identify a deadlock in a Java application?
+
+// 10. "Runnable" vs "Callable" — when would you choose each?
+
+// 11. Why would you use "ExecutorService" instead of creating threads manually?
+
+// 12. What problem does "CompletableFuture" solve?
+
+// 13. How can a Java application have a memory leak despite having Garbage Collection?
+
+// 14. Heap vs Stack — what is stored where?
+
+// 15. What happens during a Full GC, and why can it affect API latency?
+
+// 16. What happens if an exception is thrown inside a thread?
+
+// 17. Why can "HashSet" detect duplicate objects incorrectly if "equals()" and "hashCode()" are implemented badly?
+
+// 18. When would you use "Optional", and when should you avoid it?
+
+// 19. Streams vs traditional loops — when would you prefer one over the other?
+
+// 20. Your Java API suddenly becomes slow in production. What JVM-level metrics would you check first?
+
+
