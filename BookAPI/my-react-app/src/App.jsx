94219b2aceefda73d2b6c2936c96f4f34b374102
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-08 21:07:11 +0530

    <a className="nav-link" href="#Accounts.jsx">

--- BookAPI/my-react-app/src/App.jsx
+++ BookAPI/my-react-app/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
               </li>
 
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="#Accounts.jsx">
                   Accounts
                 </a>
               </li>
