9bc9940485a918f2fb68e994dfe9e8c3644814d8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-13 14:43:40 +0530

    NetflixIndexComponent()import multiple packaes

---
+++ netflix/test/src/components/NetflixIndexComponent.js
@@ -0,0 +1,22 @@
+import { NetflixHeaderComponents } from "./NetflixHeaderComponent";
+import { NetflixMainComponent } from "./NetflixMainComponent";
+import { NetflixRegisterComponent } from "./NetflixRegisterComponent";
+
+export default function NetflixIndexComponent(){
+    return(
+        <div className="container-fluid">
+            <div className="box">
+                <header>
+                    <NetflixHeaderComponents />
+                </header>
+                <section className="d-flex justify-content-center align-item-center">
+                    <main>
+                        <NetflixMainComponent />
+                        <NetflixRegisterComponent />
+                    </main>
+                    
+                </section>
+            </div>
+        </div>
+    );
+}
