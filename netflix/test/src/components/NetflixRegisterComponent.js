7bd154b72442cb186509f01a9dca90708b76ee76
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-13 15:52:53 +0530

    <button className="btn btn-danger ms-2 ">

--- netflix/test/src/components/NetflixRegisterComponent.js
+++ netflix/test/src/components/NetflixRegisterComponent.js
@@ -1,12 +1,12 @@
 export function NetflixRegisterComponent(){
     return(
         <div className="d-flex justify-content-center mt-4">
-            <div className="text-center bg-drak text-white p-5 w-50">
+            <div className="text-center bg-drak text-white p-5 w-100">
                 <p>Ready to watch? Enter your email to start your membership.</p>
                 <div>
-                    <div className="input-group input-group-lg">
+                    <div className="input-group input-group-lg d-flex align-items-center gap-2">
                         <input type="email" className="from-control" placeholder="Email Address" />
-                <button className="btn btn-danger ms-2">
+                <button className="btn btn-danger ms-2 ">
                     Try 30 Days for &#8377; 0 &nbsp; &nbsp;
                     <span className="bi bi-chevron-right"></span>
                 </button>
