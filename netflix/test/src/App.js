f1f0d10a963a88401b22bc44b8eec7f4869480de
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-13 15:52:03 +0530

    +import 'bootstrap/dist/css/bootstrap.min.css';

--- netflix/test/src/App.js
+++ netflix/test/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import './components/style.css';
-// import '../node_modules/bootstrap/dist/css/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NetflixIndexComponent from './components/NetflixIndexComponent';
 
 function App() {
