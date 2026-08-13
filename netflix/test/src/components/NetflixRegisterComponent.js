a4a7203b085a44cb99c57cc22bb253e873e11dca
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-13 13:29:24 +0530

    NetflixRegisterComponent() email and button

---
+++ netflix/test/src/components/NetflixRegisterComponent.js
@@ -0,0 +1,20 @@
+export function NetflixRegisterComponent(){
+    return(
+        <div className="d-flex justify-content-center mt-4">
+            <div className="text-center bg-drak text-white p-5 w-50">
+                <p>Ready to watch? Enter your email to start your membership.</p>
+                <div>
+                    <div className="input-group input-group-lg">
+                        <input type="email" className="from-control" placeholder="Email Address" />
+                <button className="btn btn-danger ms-2">
+                    Try 30 Days for &#8377; 0 &nbsp; &nbsp;
+                    <span className="bi bi-chevron-right"></span>
+                </button>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
