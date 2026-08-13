ed9798bd4c6764598cf187023bdf905197c6038b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-13 14:45:02 +0530

    NetflixMainComponent() netflix plains and information

---
+++ netflix/test/src/components/NetflixMainComponent.js
@@ -0,0 +1,11 @@
+export function NetflixMainComponent(){
+    return(
+        <div>
+             <div className="text-center">
+                <h1 className="custom-h1">Unlimited movies,<br /> shows, and more</h1>
+                <p>Plans start at ₹149. Cancel anytime.</p>
+
+             </div>
+        </div>
+    );
+}
