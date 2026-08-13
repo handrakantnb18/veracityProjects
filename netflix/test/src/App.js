4711c962ede54c3111f0d06f2f30029c6f14e9e9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-13 13:30:06 +0530

    renders learn react link

---
+++ netflix/test/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { NetflixRegisterComponent } from './components/NetflixRegisterComponent';
+import NetFlixIndexComponent from './components/NetflixIndexComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <NetflixRegisterComponent />
+      <NetFlixIndexComponent />
+      
+    </div>
+  );
+}
+
+export default App;
