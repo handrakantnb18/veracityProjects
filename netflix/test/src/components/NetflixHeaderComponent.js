a678266e8be8ddb358c72bb90d43ba596257dbb4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-13 15:53:26 +0530

    <button className="btn btn-danger px-4">

--- netflix/test/src/components/NetflixHeaderComponent.js
+++ netflix/test/src/components/NetflixHeaderComponent.js
@@ -1,14 +1,16 @@
 export function NetflixHeaderComponents(){
     return(
         <div>
-            <div className="d-flex justify-content-between">
-                <h1>NETFLIX</h1>
-                <div>
-                    <select className="form-select">
+            <div className="d-flex justify-content-around">
+                <h1 className="netflix">NETFLIX</h1>
+                <div className="form-op-but d-flex align-items-center gap-2">
+                    <select className="form-select bg-dark text-white">
                         <option>English</option>
                         <option>Hindi</option>
                     </select>
-                    <button className="btn btn-danger">Sign In</button>
+                    <button className="btn btn-danger px-4">
+                        Sign In
+                    </button>
                 </div>
             </div>
         </div>
