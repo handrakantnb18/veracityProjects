bc06b7c2f7d7bf0e1ab0856ea20b5c7a4d36814f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-17 16:20:05 +0530

    <p>New members only. Terms below.</p>

--- netflix/test/src/components/NetflixRegisterComponent.js
+++ netflix/test/src/components/NetflixRegisterComponent.js
@@ -6,14 +6,16 @@ export function NetflixRegisterComponent(){
                 <div>
                     <div className="input-group input-group-lg d-flex align-items-center gap-2">
                         <input type="email" className="from-control" placeholder="Email Address" />
-                <button className="btn btn-danger ms-2 ">
-                    Try 30 Days for &#8377; 0 &nbsp; &nbsp;
-                    <span className="bi bi-chevron-right"></span>
-                </button>
+                            <button className="btn btn-danger ms-2 ">
+                                 Try 30 Days for &#8377; 0 &nbsp; &nbsp;
+                                 <span className="bi bi-chevron-right"></span>
+                            </button>
                     </div>
                 </div>
-            </div>
-
+                <div>
+                    <p>New members only. Terms below.</p>
+                </div>
+            </div>             
 
         </div>
     );
