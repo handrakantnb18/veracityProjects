54d9c8df14168a71385a6115575483ea3afe9bbf
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-17 16:20:28 +0530

    +export function TrandingNowComponents(){

---
+++ netflix/test/src/components/TrendingNowComponents.js
@@ -0,0 +1,108 @@
+export function TrandingNowComponents(){
+    return(
+
+        <div className="container mt-4">
+            <h3>Trending Now</h3>
+           
+            <div className="row">
+
+                <div className="col-md-4">
+                    <div className="card">
+                        <img  
+                            src="./netfix-card.jpeg"
+                            className="card-img-top"
+                            alt="Movie 1"
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Movie One</h5>
+                            <p className="card-text">
+                                Watch the latest treding movies
+                            </p>
+                            <button className="btn btn-danger">
+                                Watch Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+                    <div className="card">
+                        <img
+                            src="https://via.placeholder.com/300x200"
+                            className="card-img-top"
+                            alt="Movie 2"
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Movie Two</h5>
+                            <p className="card-text">
+                                Enjoy popular movies and shows.
+                            </p>
+                            <button className="btn btn-danger">
+                                Watch Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+                    <div className="card">
+                        <img
+                            src="https://via.placeholder.com/300x200"
+                            className="card-img-top"
+                            alt="Movie 2"
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Movie Two</h5>
+                            <p className="card-text">
+                                Enjoy popular movies and shows.
+                            </p>
+                            <button className="btn btn-danger">
+                                Watch Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+                    <div className="card">
+                        <img
+                            src="https://via.placeholder.com/300x200"
+                            className="card-img-top"
+                            alt="Movie 2"
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Movie Two</h5>
+                            <p className="card-text">
+                                Enjoy popular movies and shows.
+                            </p>
+                            <button className="btn btn-danger">
+                                Watch Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+                    <div className="card">
+                        <img
+                            src="https://via.placeholder.com/300x200"
+                            className="card-img-top"
+                            alt="Movie 2"
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Movie Two</h5>
+                            <p className="card-text">
+                                Enjoy popular movies and shows.
+                            </p>
+                            <button className="btn btn-danger">
+                                Watch Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
+}
