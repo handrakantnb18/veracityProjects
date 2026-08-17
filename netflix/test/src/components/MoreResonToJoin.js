e5356d6af4fb85745909a6781b996153a84ad896
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-17 16:19:08 +0530

    +export function MoreResonToJoin(){

---
+++ netflix/test/src/components/MoreResonToJoin.js
@@ -0,0 +1,64 @@
+export function MoreResonToJoin(){
+    return(
+        <div className="container mt-5">
+            <h2> More reasons to join </h2>
+            <div className="row g-4">
+
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-dark text-white h-100 p-3">
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                Enjoy on your TV
+                            </h4>
+                            <p className="card-text">
+                                Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-dark text-white h-100 p-3">
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                Enjoy on your TV
+                            </h4>
+                            <p className="card-text">
+                                Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-dark text-white h-100 p-3">
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                Enjoy on your TV
+                            </h4>
+                            <p className="card-text">
+                                Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 col-lg-3">
+                    <div className="card bg-dark text-white h-100 p-3">
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                Enjoy on your TV
+                            </h4>
+                            <p className="card-text">
+                                Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                
+
+            </div>
+        </div>
+    );
+}
