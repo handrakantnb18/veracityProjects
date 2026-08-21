d33a41b25c0dbf61b355ff92017bc68f7688076c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-21 12:43:41 +0530

    function loadCategory()

---
+++ netflix/test/src/components/useEffectComponent.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+export function UseEffectComponent() {
+
+    const [categories, setCategories] = useState([]);
+
+    function loadCategory() {
+        fetch("https://fakestoreapi.com/products/categories")
+        .then(res => res.json())
+        .then(category => {
+            category.unshift("All");
+            setCategories(category);
+
+        })
+    }
+
+    useEffect(() => {
+        loadCategory();
+    })
+    return(
+        <div className="container-fluid mt-3">
+            <header className="bg-bg-primary text-white text-center p-2">Online Shopping</header>
+                <section className="row">
+                    <nav className="col-3">
+                        <h4>Select Category</h4>
+                        <select>
+                            {
+                                categories.map(c => {
+                                    <option>{c.toUppercase()}</option>
+                                })
+                            }
+                        </select>
+                    </nav>
+                    <main className="col-9 d-flex flex-wrap">
+
+                    </main>
+                </section>
+            
+        </div>
+    );
+}
