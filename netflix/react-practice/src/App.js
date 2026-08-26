144cb7ecd000d042acdc01781486b7dcdcebc1db
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-26 16:53:22 +0530

    <h1>Hello word !!!</h1>  <h1>my name is chandrakant bhosale !</h1>

--- netflix/react-practice/src/App.js
+++ netflix/react-practice/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Car from './components/Car';
+// import { FormatName } from './components/FormatName';
+import { Welcome } from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello word !!!</h1>
       <h1>my name is chandrakant bhosale !</h1>
+
+       {/* <FormatName /> */}
+
+      <Welcome />
+      <Car />
     </div>
   );
 }
