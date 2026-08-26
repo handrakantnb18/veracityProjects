281dc3f39d129a1ff649b43fde9b861f93cf775d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-26 16:55:05 +0530

    <nav > </nav>

---
+++ netflix/react-practice/src/components/Car.js
@@ -0,0 +1,7 @@
+export default function Car(props) {
+    return (
+        <div>
+            <h2>I am a {props.color} Car!</h2>
+        </div>
+    );
+}
---
+++ netflix/react-practice/src/components/Nav.js
@@ -0,0 +1,9 @@
+export function Nav() {
+
+    return(
+        <nav >
+            <h2></h2>
+        </nav>
+    );
+
+}
